c6fe63b96315b1265b389e5cfb1fa593ac1f690f
soumya.sen <soumya.sen@dnb.no>
2025-07-29 15:54:33 +0200

    Updated rabbitMQ with retry functionality

--- search-service/src/utils/rabbitmq.js
+++ search-service/src/utils/rabbitmq.js
@@ -6,18 +6,38 @@ let channel = null;
 
 const EXCHANGE_NAME = "social_media_events";
 
-async function connectToRabbitMQ(params) {
-  try {
-    connection = await amqp.connect(process.env.RABBITMQ_URL);
-    channel = await connection.createChannel();
-    await channel.assertExchange(EXCHANGE_NAME, "topic", { durable: false });
-    logger.info("Connected to rabbitMQ");
-    return channel;
-  } catch (error) {
-    logger.error("Error creating to rabbitMQ", error);
+// async function connectToRabbitMQ() {
+//   try {
+//     connection = await amqp.connect(process.env.RABBITMQ_URL);
+//     channel = await connection.createChannel();
+//     await channel.assertExchange(EXCHANGE_NAME, "topic", { durable: false });
+//     logger.info("Connected to rabbitMQ");
+//     return channel;
+//   } catch (error) {
+//     logger.error("Error creating to rabbitMQ", error);
+//   }
+// }
+async function connectToRabbitMQ(retries = 10, delay = 5000) {
+  for (let i = 0; i < retries; i++) {
+    try {
+      connection = await amqp.connect(process.env.RABBITMQ_URL);
+      channel = await connection.createChannel();
+      await channel.assertExchange(EXCHANGE_NAME, "topic", { durable: false });
+      logger.info("Connected to rabbitMQ");
+      return channel;
+    } catch (error) {
+      logger.error(
+        `RabbitMQ connection failed (attempt ${i + 1}): ${error.message}`
+      );
+      if (i < retries - 1) {
+        await new Promise((res) => setTimeout(res, delay));
+      } else {
+        logger.error("All RabbitMQ connection attempts failed. Exiting.");
+        process.exit(1);
+      }
+    }
   }
 }
-
 async function consumeEvent(routingKey, callBack) {
   if (!channel) {
     await connectToRabbitMQ();
